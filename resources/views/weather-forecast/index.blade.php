b4aacc745dcdf041329eeec91f87e1964ac83775
Jeric Dela Cruz <delacruz.jeric.j@gmail.com>
2025-04-05 21:59:32 +0800

    secret

--- resources/views/weather-forecast/index.blade.php
+++ resources/views/weather-forecast/index.blade.php
@@ -123,7 +123,7 @@
     </div>
 
     <script>
-        const API_KEY = "9505fd1df737e20152fbd78cdb289b6a";
+        const API_KEY = "";
         const form = document.getElementById("weatherForm");
         const cityInput = document.getElementById("cityInput");
         const provinceInput = document.getElementById("provinceInput");
