aba442f50c19064cf1870c3e264db64ff8a691ba
Je-ric <delacruz.jeric.j@gmail.com>
2025-05-28 21:40:44 +0800

    Refactor modal component for programs with improved structure and styling, and integrate close button functionality

--- resources/views/components/x-button.blade.php
+++ resources/views/components/x-button.blade.php
@@ -1,8 +1,8 @@
 <button type="button" onclick="this.closest('dialog')?.close()"
-    class="absolute top-3 right-3 text-gray-400 hover:text-gray-800 transition-colors duration-200 focus:outline-none z-50">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 0 20 20">
+    class="absolute top-3 right-3 text-gray-400 hover:text-[#1a2235] transition-all duration-200 focus:outline-none z-50 rounded-full border border-transparent hover:border-[#1a2235] hover:bg-gray-100 p-1.5">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current" viewBox="0 0 20 20">
         <path fill-rule="evenodd"
               d="M10 8.586l4.95-4.95a1 1 0 011.414 1.414L11.414 10l4.95 4.95a1 1 0 01-1.414 1.414L10 11.414l-4.95 4.95a1 1 0 01-1.414-1.414L8.586 10l-4.95-4.95a1 1 0 011.414-1.414L10 8.586z"
-              clip-rule="evenodd"/>
+              clip-rule="evenodd" />
     </svg>
 </button>
