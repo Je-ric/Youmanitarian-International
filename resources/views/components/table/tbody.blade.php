3d04074f0888b23fb6cda3970cd2032594623f5e
Je-ric <delacruz.jeric.j@gmail.com>
2025-07-22 16:55:36 +0800

    changing style of table data but in tbody as base

--- resources/views/components/table/tbody.blade.php
+++ resources/views/components/table/tbody.blade.php
@@ -3,12 +3,13 @@
 @php
     $baseClasses = '';
     if ($striped) {
-        $baseClasses .= ' [&>tr:nth-child(odd)>td]:bg-white';
-        $baseClasses .= ' [&>tr:nth-child(even)>td]:bg-gray-100';
+        // $baseClasses .= ' [&>tr:nth-child(odd)>td]:bg-white';
+        // $baseClasses .= ' [&>tr:nth-child(even)>td]:bg-gray-100';
+        $baseClasses .= 'bg-white';
     }
     $classes = trim($baseClasses . ' ' . $class);
 @endphp
 
 <tbody class="{{ $classes }}">
     {{ $slot }}
-</tbody> 
+</tbody>
