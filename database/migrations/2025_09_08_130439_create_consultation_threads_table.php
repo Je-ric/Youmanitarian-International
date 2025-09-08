853b74637f8bb47f156653b0f9c55fe3635dc88c
Je-ric <delacruz.jeric.j@gmail.com>
2025-09-08 13:06:02 +0800

    add migration files for consultation hours, threads, and chats

---
+++ database/migrations/2025_09_08_130439_create_consultation_threads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultation_threads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('volunteer_id');
+            $table->unsignedBigInteger('professional_id');
+            $table->enum('status', ['active', 'closed'])->default('active');
+            $table->timestamps();
+
+            $table->foreign('volunteer_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('professional_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultation_threads');
+    }
+};
